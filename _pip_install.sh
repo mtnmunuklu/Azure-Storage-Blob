e5526ce4f526a98d137edb10399dbb021cbffc33
metinmunuklu <mtnmunuklu@gmail.com>
2021-08-20 17:43:00 +0300

    Edited pip install

--- _pip_install.sh
+++ _pip_install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-
+  
 apt-get install python3-venv
 python3 -m venv venv
-
+source venv/bin/activate
 # Check you have an activated venv and you're in the right directory for it
-if [[ $VIRTUAL_ENV == $PWD'/.venv' ]]; then
+if [[ $VIRTUAL_ENV == $PWD'/venv' ]]; then
     echo "Upgrading pip in present virtual environment"
     python3 -m pip install --upgrade pip
     echo "Installing requirements in present virtual environment"
@@ -14,7 +14,7 @@ if [[ $VIRTUAL_ENV == $PWD'/.venv' ]]; then
 else
     echo "
     ---------------------------------------------------------------------------------
-        You need to have THIS directory's .venv activated to install requirements!
+        You need to have THIS directory's venv activated to install requirements!
         You are now in:    $PWD
         Your venv is from: $([ -z ${VIRTUAL_ENV+x} ] && echo "NONE" || echo "$VIRTUAL_ENV")
     ---------------------------------------------------------------------------------
